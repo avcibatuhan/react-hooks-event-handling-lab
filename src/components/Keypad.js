14a922df73bbc3483dd3089a05d5ef70c63c0616
batuhan <avcibatuhan26@gmail.com>
2022-02-01 13:07:08 +0300

    event handling lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+function Keypad (){
+    function handleChange(){
+        console.log("Entering password...");
+    }
+    return (
+        <div>
+            <input onChange={handleChange} type="password"/>
+        </div>
+    );
+}
+
+export default Keypad;
